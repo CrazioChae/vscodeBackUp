d3bdf66ff55bf124c0823dd91e885ae2e861c211
Crazio_Chae <mindroid24@gmail.com>
2023-07-12 04:18:27 +0900

    230712_동기화

---
+++ C/11.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+void main()
+{
+    int var;
+
+    var = 0x12345678;
+}
--- Python/1.py
+++ Python/1.py
@@ -1,3 +1,9 @@
-a = 12
-b = 34
-print(a, b)
+name = input("이름: ")
+print("name: ", name)
+stud_num = input("학번: ")
+
+print("학번: ", stud_num, "이름: ", name)
+
+
+
+
